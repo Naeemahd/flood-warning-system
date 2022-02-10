3c96782d3e6e33d97aed66b0d66f6627dc0443c8
Naeemah Daud <nd493@cam.ac.uk>
2022-02-10 08:26:23 +0000

    test build_lists() function

--- test_everything.py
+++ test_everything.py
@@ -1,8 +1,16 @@
-
-from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_by_distance,build_lists
 
 def test_stations_by_distance():
     list1 =((1,0), (1,0), (1,0))
     p = (0,0)
     test1 = stations_by_distance(list1, p)
     
+
+def test_build_lists():
+    #The below loop creates 3 lists, one for each of the Station names, Closest town and their coordinates
+    list_names=["test", "Test", "TEst"]
+    list_towns=["test", "Test", "TEst"]
+    list_coord=[(1,0), (1,0), (1,0)]
+    p = (0,0)
+    #This then runs the function above and finds the distance from the point
+    list_distance = (stations_by_distance(list_coord, p))    

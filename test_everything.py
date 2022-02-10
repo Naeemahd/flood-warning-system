a782e3df69e1adcb51a3a1e513384d591299fa2d
Naeemah Daud <nd493@cam.ac.uk>
2022-02-10 08:19:37 +0000

    test

--- test_everything.py
+++ test_everything.py
@@ -2,7 +2,7 @@
 from floodsystem.geo import stations_by_distance
 
 def test_stations_by_distance():
-    list1 =('(1,0)', '(1,0)', '(1,0)')
+    list1 =((1,0), (1,0), (1,0))
     p = (0,0)
     test1 = stations_by_distance(list1, p)
     

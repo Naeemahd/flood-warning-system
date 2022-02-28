845b92d810097c186c9de779297be9541fe90e6a
si356 <98694120+si356@users.noreply.github.com>
2022-02-28 23:21:50 +0000

    Update flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,3 +1,7 @@
+from floodsystem.datafetcher import fetch_measure_levels
+
+import datetime
+
 def stations_highest_rel_level(stations, N):
     #create empty lists
     rel_level = []
@@ -15,11 +19,32 @@ def stations_highest_rel_level(stations, N):
     list_tuple = list(zip(names,rel_level))    
     #sorts list by water level low to high
     list_tuple.sort(key= lambda x:x[1], reverse = True)  
+    return list_tuple[:N]
 
-    for i in range(N):
-        print("Station name and relative level: {}, {}".format(
-                list_tuple[i][0], list_tuple[i][1]))
-    #print last N terms of list_tuple 
+##### THE BELOW FUNCTION IS NOT IN USE BUT COULD BE USED TO CORRECT TASK 2E #######
+def stations_highest_rel_level_with_historical(stations, N):
+    #create empty lists
+    rel_level = []
+    names=[]
+    dt = 1
+
+    #loops through stations list and add the names and water levels
+    for i in range(len(stations)):
+        
+        if stations[i].fraction != None:
+            if str(stations[i].latest_level) != None:
+                dates, levels = fetch_measure_levels(
+                stations[i].measure_id, dt=datetime.timedelta(days=dt))
+                if dates == [] or levels == []:
+                    print("No historical data available for the current station: {}".format(stations[j].name))
+                else:
+                    rel_level.append(stations[i].fraction)
+                    names.append(stations[i].name)
+    #puts lists together    
+    list_tuple = list(zip(names,rel_level))    
+    #sorts list by water level low to high
+    list_tuple.sort(key= lambda x:x[1], reverse = True)  
+    return list_tuple[:N]
     
 def stations_level_over_threshold(stations, tol):
     over_tol = []

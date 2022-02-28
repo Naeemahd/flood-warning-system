c1751cdca801d3c8c6bac74b988ff59aab450ec6
si356 <98694120+si356@users.noreply.github.com>
2022-02-28 15:30:12 +0000

    Update Task2C.py

--- Task2C.py
+++ Task2C.py
@@ -1,10 +1,12 @@
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import relative_water_level_all
 
 def run7():    
     # Create list with all the station data
     stations = build_station_list()
     update_water_levels(stations)
+    stations = relative_water_level_all(stations)
     N=10
     stations_highest_rel_level(stations, N)
     return

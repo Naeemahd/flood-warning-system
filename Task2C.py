898e68ff142c4491a9c4f69b4e4acff5062215c7
Naeemah Daud <nd493@cam.ac.uk>
2022-02-28 15:15:05 +0000

    second bit of 2B

--- Task2C.py
+++ Task2C.py
@@ -1,8 +1,10 @@
 from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
 
 def run7():    
     # Create list with all the station data
     stations = build_station_list()
+    update_water_levels(stations)
     N=10
     stations_highest_rel_level(stations, N)
     return
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,15 +1,33 @@
 def stations_highest_rel_level(stations, N):
     #create empty lists
-    level = []
+    rel_level = []
     names=[]
+
     #loops through stations list and add the names and water levels
     for i in range(len(stations)):
-        level.append(stations[i].latest_level)
-        names.append(stations[i].name)
+        
+        if str(stations[i].latest_level) != "None":
+            rel_level.append(stations[i].fraction)
+            names.append(stations[i].name)
+          
     #puts lists together    
-    list_tuple = list(zip(names, level))    
+    list_tuple = list(zip(names,rel_level))    
     #sorts list by water level low to high
-    list_tuple.sort(key= lambda x:x[1])  
-    #print last N terms of list_tuple 
+    list_tuple.sort(key= lambda x:x[1], reverse = True)  
+
     for i in range(N):
-        print(list_tuple[(-1*i)-1]) 
+        print("Station name and relative level: {}, {}".format(
+                list_tuple[i][0], list_tuple[i][1]))
+    #print last N terms of list_tuple 
+    
+def stations_level_over_threshold(stations, tol):
+    over_tol = []
+    rel_level =[]
+    for i in range(len(stations)):
+        if str(stations[i].latest_level) != "None" and stations[i].fraction > tol:
+            over_tol.append(stations[i].name)
+            rel_level.append(stations[i].fraction)
+    list_tuple = list(zip(over_tol,rel_level))    
+    list_tuple.sort(key= lambda x:x[1], reverse = True)   
+    return(list_tuple)
+        

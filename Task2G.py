5860fa79e034a71b0f3b4eceabf7350fb156a1da
Naeemah Daud <nd493@cam.ac.uk>
2022-03-03 09:33:26 +0000

    task 2G plan

--- Task2G.py
+++ Task2G.py
@@ -1,4 +1,28 @@
 '''plan:
-make empty town list, use cam code thingy to append each station with given town to list, test water
+make empty station list, use cam code thingy to append each station with given town to list, test water
 levels at each station(if more than this then severe, etc), make boolean condition in each case
-(eg.severe=true), '''
+(eg.severe=true), for i in range len list
+
+severe: increasing and more than 2
+high increasing more than 1.4
+moderate increasing level 0.8-1
+'''
+
+
+
+'''
+get a list of the stations with highest water levels
+
+find the towns associated with these stations
+loop through list of stations, append town associated to blank list
+
+find ALL the stations associated with the towns
+loop through stations, if stations.town = current town
+    add to list
+
+
+test the stations water level (compared today vs yesterday)
+
+
+
+'''

5bee03774e1deae5af0b12af014b513c143cec84
si356 <98694120+si356@users.noreply.github.com>
2022-03-03 08:16:27 +0000

    Update plot.py

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
-
-def plot_water_level_with_fit(station, dates, levels, p):
-
-    x = matplotlib.dates.date2num(dates)
-
-    x_c = x
-    y = levels
-    # Find coefficients of best-fit polynomial f(x) of degree p
-    p_coeff = np.polyfit(x_c-x[0], y, p)
-
-    # Convert coefficient into a polynomial that can be evaluated,
-    # e.g. poly(0.3)
-    poly = np.poly1d(p_coeff)
-    # Plot original data points
-    plt.plot(x_c, y, '.')
-
-    # Plot polynomial fit at 30 points along interval
-    x1 = np.linspace(x[0], x[-1], 30)
-    plt.plot(x1, poly(x1 - x[0]))
-
-    # Display plot
-    plt.show()
-=======
 import matplotlib.pyplot as plt
 
 def plot_water_levels(station, dates, levels):
@@ -45,7 +18,7 @@ def plot_water_levels(station, dates, levels):
     # Add axis labels, rotate date labels and add plot title
     plt.xlabel('date')
     plt.ylabel('water level (m)')
-    plt.xticks(rotation=45);
+    plt.xticks(rotation=45)
     plt.title(station.name)
     plt.legend()
 
@@ -54,5 +27,3 @@ def plot_water_levels(station, dates, levels):
 
     plt.show()
     return
-
->>>>>>> 97a417ccc4b50258945e52b0e3069cf5f3bbefa3
